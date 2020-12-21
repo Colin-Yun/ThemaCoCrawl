4d7ba35e000f3e908c9b36eef24e97d6c894d71e
Colin-Yun <doit_again@naver.com>
2020-12-21 15:21:17 +0900

    column 항목 신규 추가

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ import pandas as pd
 import os
 from datetime import date
 
-chrome_path = "C:\Program Files\Google\Chrome\Application\chromedriver"
+#chrome_path = "C:\Program Files\Google\Chrome\Application\chromedriver"
+chrome_path = "C:\Program Files\Google\Chrome\Application\chromedriver\chromedriver.exe"
 thema_page = """https://finance.naver.com/sise/theme.nhn?&page={0}"""
 NAVI_PAGE_NUM = 6
-col_lab_1 = ["테마", "기업명", "편입사유", "시가총액(억)", "영업이익(억)", "영업이익증가율(%)", "외국인비중(%)", "PER"]
+col_lab_1 = ["테마", "기업명", "편입사유", "시가총액(억)", "매출액(억)", "영업이익(억)", "영업이익률(%)", "영업이익증가율(%)", "외국인비중(%)", "PER"]
 
 '''************************************************
 * @Function Name : get_page
 ************************************************'''
 def get_page(url):
     with requests.Session() as sess:
-        page = sess.get(url)
+        page = sess.get(url, verify=False)
         html = page.text
     return html
 
@@ -69,12 +70,13 @@ def get_page_selenium(url, driver):
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[1]/td[3]/label').click()                        #거래대금 체크해제
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[2]/td[2]/label').click()                        #매도호가 체크해제
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[2]/td[3]/label').click()                        #전일거래량 체크해제
-
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[1]/td[4]/label').click()                        #시가총액 체크
+
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[1]/td[5]/label').click()                        #영억이익(억) 체크
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[1]/td[6]/label').click()                        #PER 체크
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[2]/td[5]/label').click()                        #영업이익증가율 체크
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[3]/td[3]/label').click()                        #외국인비율 체크
+    driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/table/tbody/tr[4]/td[4]/label').click()                        #매출액(억) 체크
 
     driver.find_element_by_xpath('//*[@id="contentarea"]/div[3]/form/div/div/div/a[1]/img').click()                                         #적용하기 체크
     html = driver.page_source
@@ -97,7 +99,7 @@ def proc_get_coinfo(themas):
         res = get_thema_coinfo(html, thema.thema_name)
 
         # 테마별 기업리스트 개별 저장
-        save_to_excel(res, col_lab_1, (thema.thema_name).replace('/', ''), today)
+        #save_to_excel(res, col_lab_1, (thema.thema_name).replace('/', ''), today)
 
         merge_lst = merge_lst + res
         res.clear()
@@ -108,6 +110,20 @@ def proc_get_coinfo(themas):
     return
 
 
+
+'''************************************************
+* @Function Name : calc_profit_rate
+************************************************'''
+def calc_profit_rate(sales, profit):
+    try :
+        profit_rate = (profit / sales) * 100
+        profit_rate = round(profit_rate, 2)
+    except:
+        profit_rate = ""
+
+    return profit_rate
+
+
 '''************************************************
 * @Function Name : get_thema_coinfo
 ************************************************'''
@@ -115,11 +131,12 @@ def get_thema_coinfo(html, thema_name):
     soup = bs(html, 'html.parser')
     soups = soup.findAll('tr', {'onmouseover': 'mouseOver(this)'})
 
-    AVSL = 3                #시가총액
-    B_PROFIT = 4            #영업이익
-    B_PROFIT_RATE = 5       #영업이익증가율
-    FORIGN_RATE = 6         #외국인비중
-    PER = 7                 #PER
+    AVSL = 3                        #시가총액
+    SALES =  4                      #매출액
+    B_PROFIT = 5                    #영업이익
+    B_PROFIT_RAISE_RATE = 6         #영업이익증가율
+    FORIGN_RATE = 7         #외국인비중
+    PER = 8                 #PER
 
     ret_lst = []
     for el in soups:
@@ -130,8 +147,29 @@ def get_thema_coinfo(html, thema_name):
         info_vals = el.findAll('td', {'class': 'number'})
 
         #co_info = CoInfo(thema_name, info_co_name, info_txt, info_vals[AVSL], info_vals[B_PROFIT], info_vals[B_PROFIT_RATE], info_vals[FORIGN_RATE], info_vals[PER])
-        co_info_lst = [thema_name, info_co_name, info_txt, int(info_vals[AVSL].get_text().replace(',','')), info_vals[B_PROFIT].get_text(),
-                       info_vals[B_PROFIT_RATE].get_text(), info_vals[FORIGN_RATE].get_text(), info_vals[PER].get_text()]
+        print(info_co_name)
+
+        try:
+            sales_acc = int(info_vals[SALES].get_text().replace(',',''))
+            b_profit = int(info_vals[B_PROFIT].get_text().replace(',',''))
+            per = float(info_vals[PER].get_text().replace(',',''))
+
+        except:
+            sales_acc = 0
+            b_profit = 0
+            per = 0
+
+        co_info_lst = [thema_name,
+                       info_co_name,        #기업명
+                       info_txt,            #편입사유
+                       int(info_vals[AVSL].get_text().replace(',','')),     #시가총액
+                       sales_acc,                                           #매출액
+                       info_vals[B_PROFIT].get_text(),                      #영업이익
+                       calc_profit_rate(sales_acc, b_profit),       #영업이익률
+                       info_vals[B_PROFIT_RAISE_RATE].get_text(),   #영업이익증가율
+                       float(info_vals[FORIGN_RATE].get_text()),           #외국인비중
+                       per                    #PER
+                       ]
         ret_lst.append(co_info_lst)
 
     return ret_lst
